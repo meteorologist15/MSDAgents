ee43509eeae5c582d62b62e5d4111336461816ee
Kristopher Rand <Kristopher.Rand@noaa.gov>
2026-05-19 20:34:23 -0400

    Allow individual module docstring extractions

--- documentation-parsers/fre_parser/fre_database.py
+++ documentation-parsers/fre_parser/fre_database.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import copy
 
 from parser import ModuleDocument, CommandDocument
@@ -66,10 +68,24 @@ class FreDatabase:
         "fre.run.frerun": None
     }
 
-    def __init__(self):
+    def __init__(self, module_name=None):
         """Constructor"""
-        self.tools_dict = copy.deepcopy(self.TOOLS_DICT)
-        self.commands_dict = copy.deepcopy(self.COMMANDS_DICT)
+        module_found = False
+        if module_name:
+            for mod in self.TOOLS_DICT.keys():
+                if module_name == mod:
+                    self.tools_dict = {}
+                    self.commands_dict = {}
+                    self.tools_dict[mod] = self.TOOLS_DICT[mod]
+                    self.commands_dict[f"fre.{mod}.fre{mod}"] = self.COMMANDS_DICT[f"fre.{mod}.fre{mod}"]\
+                                                                if mod != "list"\
+                                                                else self.COMMAND_DICT[f"fre.{mod}_.fre{mod}"]
+                    module_found = True
+                    break
+
+        if not module_found or not module_name:
+            self.tools_dict = copy.deepcopy(self.TOOLS_DICT)
+            self.commands_dict = copy.deepcopy(self.COMMANDS_DICT)
 
     def summarize(self):
         """Summarizes the docstrings in the modules and commands"""
@@ -90,7 +106,7 @@ class FreDatabase:
                 self.commands_dict[command] = commanddocument
             except Exception as exc:
                 print(f"Skipping command group {command}: {exc}")
-        
+
     def to_chromadb(self):
         """Converts the summarized data into document_list, metadata_list, and id_list for ChromaDB"""
         
@@ -116,6 +132,7 @@ class FreDatabase:
         return document_list, metadata_list, id_list
 
 if __name__ == '__main__':
+
     fre_db = FreDatabase()
     fre_db.summarize()
     document_list, metadata_list, id_list = fre_db.to_chromadb()
